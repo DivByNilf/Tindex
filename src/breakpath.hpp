45f2bf882086cbab1072dc4e6c75c517638566d3
Nilf <nilfynilfy@gmail.com>
2023-03-01 10:22:27 +0200

    Breakpath cpp draft

---
+++ src/breakpath.hpp
@@ -0,0 +1,7 @@
+std::string ExtractPathExtension(const std::string &path);
+
+int breakpath(char *path, char *edname, char *efname, char *eexname);
+
+int breakpathdf(char *path, char *edname, char *efname);
+
+int breakfname(char *ifname, char *efname, char *eexname);
